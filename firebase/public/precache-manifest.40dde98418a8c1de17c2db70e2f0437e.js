96350a0a628f6cbe9360bc9084a569062ed02c84
xper28 <ahmad.hassan1@ucalgary.ca>
2019-09-28 17:24:36 -0600

    landing

---
+++ firebase/public/precache-manifest.40dde98418a8c1de17c2db70e2f0437e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98f57956227bef9bf20b6111d349a68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c745f305eef1028cb5be",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7515e2aead8438c057a5",
+    "url": "/static/js/2.0a1606ff.chunk.js"
+  },
+  {
+    "revision": "c745f305eef1028cb5be",
+    "url": "/static/js/main.be25f6dd.chunk.js"
+  },
+  {
+    "revision": "01ab9b853e6bc6bd9ef2",
+    "url": "/static/js/runtime-main.c1fc55e6.js"
+  },
+  {
+    "revision": "be0025618e74314eae1ec1cfda9d8b07",
+    "url": "/static/media/cube.be002561.svg"
+  }
+]);
